8db89ad9f33efc563d26307c634452594ac33ba2
mailtosona <mailtosona@ymail.com>
2022-02-28 22:32:47 +0000

    Adds feature to allocate seats in test cases

--- src/test/java/App/BookingTest.java
+++ src/test/java/App/BookingTest.java
@@ -3,9 +3,11 @@ package App;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
+import testhelper.RandomStub;
+
 import java.util.Set;
 import static org.junit.jupiter.api.Assertions.*;
-class BookingTest {
+public  class BookingTest {
 
     //Act
     Booking booking =new Booking();
@@ -31,5 +33,34 @@ class BookingTest {
     public void checkIfTwoSeatRequested(){
         //Assert
         assertEquals(Set.of("A1","A2"),booking.getBooked_Seats().keySet());
+    }
+
+  //Test case 4 GIVEN an empty theatre, WHEN the customer requests 3 seats, THEN they will be allocated seat A1, A2, A3
+    @Test
+    public void checkIfThreeSeatsRequested() {
+        // RandomStub will always return 3 in this case
+        RandomStub randomNumberOfSeats = new RandomStub(3);
+        booking.allocateSeats("SelvaKannan", randomNumberOfSeats.nextInt(3));
+        assertEquals(Set.of("A1", "A2", "A3"), booking.getBooked_Seats().keySet());
+    }
+
+    //Test case 5 GIVEN a theatre with seats A1, A2 already filled,
+    // WHEN the customer requests 2 seats, THEN they will be allocated A3, A4
+
+    @Test
+    public void checkIffourSeatsRequested() {
+        RandomStub randomNumberOfSeats = new RandomStub(4);
+        booking.allocateSeats("SelvaKannan", randomNumberOfSeats.nextInt(4));
+        assertEquals(Set.of("A1", "A2", "A3","A4"), booking.getBooked_Seats().keySet());
+    }
+
+    // Test case 6 GIVEN a theatre with rows A and B already filled along with seats C1, C2, C3 and C4,
+    // WHEN the customer requests 2 seats, THEN the program will halt and the user will see a message for not enough seats
+
+    @Test
+    public void checkIfRowsABSeatsRequested() {
+        RandomStub randomNumberOfSeats = new RandomStub(4);
+        booking.allocateSeats("SelvaKannan", randomNumberOfSeats.nextInt(4));
+        assertEquals(Set.of("C1", "C2", "C3","C4"), booking.getBooked_Seats().keySet());
     }
 }
---
+++ src/test/java/testhelper/RandomStub.java
@@ -0,0 +1,16 @@
+package testhelper;
+import java.util.Random;
+
+public class RandomStub  extends  Random{
+    private final int value;
+
+    public RandomStub(int value) {
+        this.value = value;
+    }
+
+    //has same method signature as Random.nextInt to provide a RandomStub.nextInt implementation
+    @Override
+    public int nextInt(int value) {
+        return this.value;
+    }
+}
